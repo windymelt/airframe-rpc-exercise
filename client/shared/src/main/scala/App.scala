8b5a2ee985063785b4039cd93ccd483bc7eb7cca
windymelt <windymelt@3qe.us>
2023-11-05 21:10:15 +0900

    we should split directory because `client` uses CrossType.Full

---
+++ client/shared/src/main/scala/App.scala
@@ -0,0 +1,7 @@
+package io.github.windymelt.airframeexercise.client
+
+import scala.scalajs.js.annotation.*
+
+object ClientMain:
+  @main def main(): Unit =
+    println("Hello, Scala.js!")
--- client/src/main/scala/io.github.windymelt.airframeexercise/client/App.scala
+++
@@ -1,4 +0,0 @@
-package io.github.windymelt.airframeexercise.client
-
-@main def main(): Unit =
-  println("hello, Scala.js!")
