517c10e3bd5f80fc12f20d980bf85fca2e8e402d
windymelt <windymelt@3qe.us>
2023-11-07 00:22:49 +0900

    removed unused import

--- server/src/main/scala/io.github.windymelt.airframeexercise/api/v1/App.scala
+++ server/src/main/scala/io.github.windymelt.airframeexercise/api/v1/App.scala
@@ -1,7 +1,5 @@
 package io.github.windymelt.airframeexercise.api.v1
 
-import wvlet.airframe.http._
-
 class MyServiceImpl extends MyService:
   override def getUserById(id: Long): Option[User] = id match
     case 42L =>
